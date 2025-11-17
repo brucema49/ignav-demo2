b52ad77d4c7c24402425e84a5a4243c7cf12f7e4
mxl <2711710007@qq.com>
2025-11-17 10:21:18 +0800

    init-ignsv-for-spoofing-detector

---
+++ build/CMakeFiles/lc-rts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/lc-rts"
+  "../bin/lc-rts.pdb"
+  "CMakeFiles/lc-rts.dir/src/ins-gnss/app/lc-rts.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lc-rts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/navapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/navapp"
+  "../bin/navapp.pdb"
+  "CMakeFiles/navapp.dir/src/ins-gnss/app/rtkrcv.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/navapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

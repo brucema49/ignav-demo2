8491a767c3761ebb1c450b97987f4eb59ee7b7cf
mxl <2711710007@qq.com>
2026-06-02 20:35:08 +0800

    修复bug

--- src/ins-gnss/rtkcmn.cc
+++ src/ins-gnss/rtkcmn.cc
@@ -1561,7 +1561,7 @@ extern int filter(double *x, double *P, const double *H, const double *v,
     double *x_,*xp_,*P_,*Pp_,*H_;
     int i,j,k,info,*ix;
 
-    ix=imat(n,1); for (i=k=0;i<n;i++) if (x[i]!=0.0&&P[i+i*n]>0.0) ix[k++]=i;
+    ix=imat(n,1); for (i=k=0;i<n;i++) if (P[i+i*n]>0.0) ix[k++]=i;
     x_=mat(k,1); xp_=mat(k,1); P_=mat(k,k); Pp_=mat(k,k); H_=mat(k,m);
     for (i=0;i<k;i++) {
         x_[i]=x[ix[i]];
